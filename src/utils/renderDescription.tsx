9e47bd61d31dc8c9efa3160336a0d70d9df5fcdf
scohen-trinity <scohencs@gmail.com>
2026-05-11 21:12:14 -0700

    feat: added utils for rendering inline links

---
+++ src/utils/renderDescription.tsx
@@ -0,0 +1,13 @@
+// Supports [link text](url) syntax in plain strings
+export function renderDescription(text: string) {
+  const parts = text.split(/(\[.*?\]\(.*?\))/g);
+
+  return parts.map((part, i) => {
+    const match = part.match(/\[(.*?)\]\((.*?)\)/);
+    if (match) {
+      const [, label, href] = match;
+      return <a key={i} href={href}>{label}</a>;
+    }
+    return part;
+  });
+}
